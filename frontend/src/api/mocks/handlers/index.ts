ca31614b5d9512cdc18cffa09b59cbddddfb3764
Mathys P.K <mathyspaillekrobb.mkp@gmail.com>
2026-09-15 19:00:11 +0200

    feat(locations, index): add stateful MSW handlers for the stocks resource

--- frontend/src/api/mocks/handlers/index.ts
+++ frontend/src/api/mocks/handlers/index.ts
@@ -3,5 +3,6 @@ import {categoryHandlers} from './categories'
 import {locationHandlers} from './locations'
 import { supplierHandlers } from './suppliers'
 import { userHandlers } from './users'
+import { stockHandlers } from './stocks'
 
-export const handlers = [...authHandlers, ...categoryHandlers, ...locationHandlers, ...supplierHandlers, ...userHandlers]
+export const handlers = [...authHandlers, ...categoryHandlers, ...locationHandlers, ...supplierHandlers, ...userHandlers, ...stockHandlers]
