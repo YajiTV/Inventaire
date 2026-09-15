dc1242030328b218807755a1df46111a6fe0c62c
Mathys P.K <mathyspaillekrobb.mkp@gmail.com>
2026-09-15 17:58:29 +0200

    feat(categroies, index): add stateful MSW handlers for the categories resource

--- frontend/src/api/mocks/handlers/index.ts
+++ frontend/src/api/mocks/handlers/index.ts
@@ -1,3 +1,4 @@
 import { authHandlers } from './auth'
+import {categoryHandlers} from './categories'
 
-export const handlers = [...authHandlers]
+export const handlers = [...authHandlers, ...categoryHandlers]
