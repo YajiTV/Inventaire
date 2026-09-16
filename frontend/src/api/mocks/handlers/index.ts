44e0c9f625f3c1fe768984c9c409d631fbb2b225
Mathys P.K <mathyspaillekrobb.mkp@gmail.com>
2026-09-16 09:21:40 +0200

    feat(replenishment, index): add stateful MSW handlers for the replenishment resource

--- frontend/src/api/mocks/handlers/index.ts
+++ frontend/src/api/mocks/handlers/index.ts
@@ -7,5 +7,6 @@ import { stockHandlers } from './stocks'
 import { productHandlers } from './products'
 import { purchaseOrderHandlers } from './purchase-orders'
 import { stockMovementHandlers } from './stock-movements'
+import { replenishmentHandlers } from './replenishment'
 
-export const handlers = [...authHandlers, ...categoryHandlers, ...locationHandlers, ...supplierHandlers, ...userHandlers, ...stockHandlers, ...productHandlers, ...purchaseOrderHandlers, ...stockMovementHandlers]
+export const handlers = [...authHandlers, ...categoryHandlers, ...locationHandlers, ...supplierHandlers, ...userHandlers, ...stockHandlers, ...productHandlers, ...purchaseOrderHandlers, ...stockMovementHandlers, ...replenishmentHandlers]
